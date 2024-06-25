a9b0efe8e46152a59553d4df3c113a6ff9847768
manibalaji003 <manibalaji22003@gmail.com>
2024-06-25 22:20:07 +0530

    hello world

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Weather-App/',  // Ensure this is the correct base path for your deployment
   plugins: [react()],
-  base: "/Weather-App/"
-})
+});
